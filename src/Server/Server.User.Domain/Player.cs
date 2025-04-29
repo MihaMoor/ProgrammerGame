e32a1444b7a886a01c26e79d1ce37957d1102cbb
Моор Михаил Александрович <m.moor@geomix.ru>
2025-04-29 11:34:59 +0700

    update(server): добавлена дока
    #20

--- src/Server/Server.User.Domain/Player.cs
+++ src/Server/Server.User.Domain/Player.cs
@@ -2,8 +2,20 @@
 
 public class Player
 {
+    /// <summary>
+    /// Id
+    /// </summary>
     public Guid Id { get; set; }
+    /// <summary>
+    /// Имя
+    /// </summary>
     public string Name { get; set; }
+    /// <summary>
+    /// Количество рабочих мест
+    /// </summary>
     public uint WorkPlaceCount { get; set; }
+    /// <summary>
+    /// Наличие работоспособного компьютера
+    /// </summary>
     public bool HaveWorkableComputer { get; set; }
 }
