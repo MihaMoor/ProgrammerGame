0400afca3306eba58d8ff6b6c99f786e24f2a489
Моор Михаил Александрович <m.moor@geomix.ru>
2025-05-07 14:32:13 +0700

    wip(server): добавлены интерфесы CQRS и репозитория
    #25

--- src/Server/Modules/Player/Module.Player.Application/GetPlayerQuery.cs
+++ src/Server/Modules/Player/Module.Player.Application/GetPlayerQuery.cs
@@ -1,29 +1,126 @@
 ﻿namespace Module.Player.Application;
 
-public interface IPlayerWriteRepository { }
-public interface IPlayerReadRepository { }
-
-public interface ICommandHandler { }
-public interface IQuery { }
-public interface IQueryHandler<Tout, Tin> where Tin : IQuery
+public class GetPlayerQuery(IPlayerRepository playerRepository) : IQuery<Player>
 {
-    Task<Tout> Handle(Tin query, CancellationToken roken = default);
-}
-
-public class GetPlayerQuery : IQuery
-{
-    public Guid Id { get; set; }
-}
-
-public class GetPlayerQueryHandler(IPlayerWriteRepository writeRepository) : IQueryHandler<Player, GetPlayerQuery>
-{
-    public Task<Player> Handle(GetPlayerQuery query, CancellationToken roken = default)
+    public async Task<Result> Handle(Guid playerId, CancellationToken token)
     {
-        throw new NotImplementedException();
+        Player? player = await playerRepository.GetAsync(playerId, token);
+        if(player is null)
+        {
+            return Result.Failure(PlayerError.NotFound(playerId));
+        }
+        return Result.Success(player);
     }
 }
 
 public class Player
 {
+}
 
+public static class PlayerError
+{
+    public static Error NotFound(Guid playerId) => new(
+        "Player.NotFound", $"The user with the Id = '{playerId}' was not found");
+}
+
+public interface IPlayerRepository
+{
+    /// <summary>
+    /// Получение игрока по Id.
+    /// </summary>
+    /// <param name="id">Id игрока</param>
+    /// <param name="cancellationToken">Токен отмены</param>
+    /// <returns>Игрока</returns>
+    Task<Player?> GetAsync(Guid id, CancellationToken cancellationToken = default);
+}
+
+public interface ICommand : IBaseCommand
+{
+}
+
+public interface ICommand<TResponse> : IBaseCommand
+{
+}
+
+public interface IBaseCommand
+{
+}
+
+public interface IQuery<TResponse>
+{
+}
+
+public interface IQueryHandler<in TQuery, TResponse>
+    where TQuery : IQuery<TResponse>
+{
+    Task<Result<TResponse>> Handle(TQuery query, CancellationToken cancellationToken);
+}
+
+public interface ICommandHandler<in TCommand>
+    where TCommand : ICommand
+{
+    Task<Result> Handle(TCommand command, CancellationToken cancellationToken);
+}
+
+public interface ICommandHandler<in TCommand, TResponse>
+    where TCommand : ICommand<TResponse>
+{
+    Task<Result<TResponse>> Handle(TCommand command, CancellationToken cancellationToken);
+}
+
+public class Result
+{
+    protected internal Result(bool isSuccess, Error error)
+    {
+        if (isSuccess && error != Error.None ||
+            !isSuccess && error == Error.None)
+        {
+            throw new ArgumentException("Invalid error", nameof(error));
+        }
+
+        IsSuccess = isSuccess;
+        Error = error;
+    }
+
+    public bool IsSuccess { get; }
+
+    public bool IsFailure => !IsSuccess;
+
+    public Error Error { get; }
+
+    public static Result Success() => new(true, Error.None);
+
+    public static Result Failure(Error error) => new(false, error);
+
+    public static Result<TValue> Success<TValue>(TValue value) => new(value, true, Error.None);
+
+    public static Result<TValue> Failure<TValue>(Error error) => new(default, false, error);
+}
+
+public class Result<TValue> : Result
+{
+    private readonly TValue? _value;
+
+    protected internal Result(TValue? value, bool isSuccess, Error error)
+        : base(isSuccess, error)
+    {
+        _value = value;
+    }
+
+    public TValue Value => IsSuccess
+        ? _value!
+        : throw new InvalidOperationException("The value of a failure result can't be accessed");
+
+    public static implicit operator Result<TValue>(TValue? value) =>
+        value is not null ? Success(value) : Failure<TValue>(Error.NullValue);
+}
+
+public record Error(string Code, string Description)
+{
+    public static readonly Error None = new(string.Empty, string.Empty);
+    public static readonly Error NullValue = new("Error.NullValue", "Null value was provided");
+
+    public static implicit operator Result(Error error) => Result.Failure(error);
+
+    public Result ToResult() => Result.Failure(this);
 }
--- src/Server/Modules/Player/Module.Player.Application/ICommand.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace Module.Player.Application;
-
-public interface ICommand
-{
-}
