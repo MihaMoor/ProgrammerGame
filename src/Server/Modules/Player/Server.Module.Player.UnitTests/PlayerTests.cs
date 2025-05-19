41cfc9ef10b379bf179501eaa06028b25744d7b8
Моор Михаил Александрович <m.moor@geomix.ru>
2025-05-19 12:51:02 +0700

    fix: исправлены тесты
    #25

--- src/Server/Modules/Player/Server.Module.Player.UnitTests/PlayerTests.cs
+++ src/Server/Modules/Player/Server.Module.Player.UnitTests/PlayerTests.cs
@@ -1,14 +1,12 @@
 ﻿using Server.Module.Player.Domain;
 using Server.Shared.Errors;
-using Xunit.Abstractions;
-using Xunit.Sdk;
 
 namespace Server.Module.Player.UnitTests;
 
 [Trait("Category", "Unit")]
 public partial class PlayerTests
 {
-    [TimeoutTheory(timeoutMilliseconds: 3)]
+    [Theory]
     [MemberData(nameof(CreateMainStatsData))]
     public void Создания_основных_характеристик(string name, bool expected, Error? error)
     {
@@ -19,79 +17,5 @@ public partial class PlayerTests
         {
             Assert.Equal(error, actual.Error);
         }
-    }
-}
-
-// Атрибут для использования в тестах
-[AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
-[XunitTestCaseDiscoverer(
-    "Server.Module.Player.UnitTests.TimeoutTestCaseDiscoverer",
-    "Server.Module.Player.UnitTests"
-)]
-public class TimeoutTheoryAttribute(int timeoutMilliseconds) : TheoryAttribute
-{
-    public int TimeoutMilliseconds { get; } = timeoutMilliseconds;
-}
-
-public class TimeoutTestCaseDiscoverer : IXunitTestCaseDiscoverer
-{
-    [Obsolete]
-    public IEnumerable<IXunitTestCase> Discover(
-        ITestMethod testMethod,
-        IAttributeInfo factAttribute
-    )
-    {
-        int timeout = factAttribute.GetNamedArgument<int>("TimeoutMilliseconds");
-        yield return new TimeoutTestCase(testMethod, timeout);
-    }
-
-    // Используем перегрузку метода, которая соответствует интерфейсу IXunitTestCaseDiscoverer,
-    // но для обратной совместимости делегируем вызов более простой версии
-    [Obsolete]
-    public IEnumerable<IXunitTestCase> Discover(
-        ITestFrameworkDiscoveryOptions discoveryOptions,
-        ITestMethod testMethod,
-        IAttributeInfo factAttribute
-    ) => Discover(testMethod, factAttribute);
-}
-
-public class TimeoutTestCase : XunitTheoryTestCase
-{
-    [Obsolete(
-        "Called by the de-serializer; should only be called by deriving classes for de-serialization purposes"
-    )]
-    public TimeoutTestCase() { }
-
-    [Obsolete]
-    public TimeoutTestCase(ITestMethod testMethod, int timeoutMilliseconds)
-        : base(
-            diagnosticMessageSink: new NullMessageSink(),
-            testMethod: testMethod,
-            defaultMethodDisplay: TestMethodDisplay.Method
-        ) => TimeoutMilliseconds = timeoutMilliseconds;
-
-    public int TimeoutMilliseconds { get; }
-
-    public override async Task<RunSummary> RunAsync(
-        IMessageSink diagnosticMessageSink,
-        IMessageBus messageBus,
-        object[] constructorArguments,
-        ExceptionAggregator aggregator,
-        CancellationTokenSource cancellationTokenSource
-    )
-    {
-        using CancellationTokenSource timeoutCts = CancellationTokenSource.CreateLinkedTokenSource(
-            cancellationTokenSource.Token
-        );
-
-        timeoutCts.CancelAfter(TimeoutMilliseconds);
-
-        return await base.RunAsync(
-            diagnosticMessageSink,
-            messageBus,
-            constructorArguments,
-            aggregator,
-            timeoutCts
-        );
     }
 }
